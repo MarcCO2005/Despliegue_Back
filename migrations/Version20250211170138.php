33d5a30c34e03e4cb82a77f635ae7a870e344f58
Marc Cuenca Olmos <mcuencaolmos@gmail.com>
2025-02-17 11:39:56 +0100

    Configuración inicial del proyecto Symfony con Doctrine y API Platform

--- migrations/Version20250211170138.php
+++ migrations/Version20250211170138.php
@@ -19,7 +19,7 @@ final class Version20250211170138 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // Usando sintaxis de MySQL
+        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE comentarios (id INT AUTO_INCREMENT NOT NULL, usuario_id INT DEFAULT NULL, eventos_id INT DEFAULT NULL, comentario VARCHAR(255) NOT NULL, INDEX IDX_F54B3FC0DB38439E (usuario_id), INDEX IDX_F54B3FC07F243861 (eventos_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE eventos (id INT AUTO_INCREMENT NOT NULL, titulo VARCHAR(255) NOT NULL, descripcion VARCHAR(255) NOT NULL, imagen VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE comentarios ADD CONSTRAINT FK_F54B3FC0DB38439E FOREIGN KEY (usuario_id) REFERENCES socios (id)');
@@ -28,6 +28,7 @@ final class Version20250211170138 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
+        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE comentarios DROP FOREIGN KEY FK_F54B3FC0DB38439E');
         $this->addSql('ALTER TABLE comentarios DROP FOREIGN KEY FK_F54B3FC07F243861');
         $this->addSql('DROP TABLE comentarios');
